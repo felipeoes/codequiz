761cfd2d08e2f500d90934768712df2e8ce0d124
Felipe Oliveira <felipeoes@usp.br>
2021-01-25 23:17:18 -0300

    Started project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,70 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import { useRouter } from 'next/router'
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+const Link = ({ children, href }) => {
+  const router = useRouter()
+  return (
+    <a
+      href="#"
+      onClick={(e) => {
+        e.preventDefault()
+        router.push(href)
+      }}
+    >
+      {children}
+      <style jsx>{`
+        a {
+          margin-right: 10px;
+        }
+      `}</style>
+    </a>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <div>
+            <Link href="/quiz">Ver quizes</Link>
+            </div>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/felipeoes" />
+    </QuizBackground>
+  );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+
+export default function Footer(props) {
+  return (
+        <a href="https://www.alura.com.br/">
+          <span>Imersão React da Alura</span>
+        </a>
+  );
+}
