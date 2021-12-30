4e71cfde3e0abde48612fbb966521d39edd8b190
Felipe Oliveira <felipeoes@usp.br>
2021-12-30 12:34:09 -0300

    added google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 /* eslint-disable react/jsx-no-bind */
-import React from 'react';
-import styled from 'styled-components';
-import Head from 'next/head';
-import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
-import ReactTooltip from 'react-tooltip';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizContainer from '../src/components/QuizContainer';
-import QuizBackground from '../src/components/QuizBackground';
-import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner';
-import Input from '../src/components/Input';
-import Button from '../src/components/Button';
+import React from "react";
+import styled from "styled-components";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+import ReactTooltip from "react-tooltip";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizContainer from "../src/components/QuizContainer";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import Input from "../src/components/Input";
+import Button from "../src/components/Button";
 // eslint-disable-next-line import/no-named-as-default
-import { QuizLogoIndex } from '../src/components/QuizLogo';
+import { QuizLogoIndex } from "../src/components/QuizLogo";
 
 const MainWidget = styled.div`
   &:hover,
@@ -29,8 +29,10 @@ const MainWidget = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
-  transition: .3s;
-  h1, h2, h3 {
+  transition: 0.3s;
+  h1,
+  h2,
+  h3 {
     font-size: 14px;
     font-weight: 700;
     line-height: 1;
@@ -44,36 +46,76 @@ const MainWidget = styled.div`
 `;
 
 // eslint-disable-next-line import/no-mutable-exports
-export let getName = '';
+export let getName = "";
 
 export default function Home() {
   const router = useRouter();
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState("");
   // eslint-disable-next-line no-const-assign
   getName = name;
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
         <meta name="title" content="Quiz Stranger Things" />
-        <meta name="description" content="Teste os seus conhecimentos sobre Stranger Things e vamos ver se você vai acabar parando no mundo invertido" />
+        <meta
+          name="description"
+          content="Teste os seus conhecimentos sobre Stranger Things e vamos ver se você vai acabar parando no mundo invertido"
+        />
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://codequiz.felipeoes.vercel.app/" />
+        <meta
+          property="og:url"
+          content="https://codequiz.felipeoes.vercel.app/"
+        />
         <meta property="og:title" content="Quiz Stranger Things" />
-        <meta property="og:description" content="Teste os seus conhecimentos sobre Stranger Things e vamos ver se você vai acabar parando no mundo invertido" />
-        <meta property="og:image" content="https://i.ibb.co/LJXFBwQ/bgImage.png?resize=1530%2C1220&ssl=1" />
+        <meta
+          property="og:description"
+          content="Teste os seus conhecimentos sobre Stranger Things e vamos ver se você vai acabar parando no mundo invertido"
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/LJXFBwQ/bgImage.png?resize=1530%2C1220&ssl=1"
+        />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://codequiz.felipeoes.vercel.app/" />
+        <meta
+          property="twitter:url"
+          content="https://codequiz.felipeoes.vercel.app/"
+        />
         <meta property="twitter:title" content="Quiz Stranger Things" />
-        <meta property="twitter:description" content="Teste os seus conhecimentos sobre Stranger Things e vamos ver se você vai acabar parando no mundo invertido" />
-        <meta property="twitter:image" content="https://i.ibb.co/LJXFBwQ/bgImage.png?resize=1530%2C1220&ssl=1" />
+        <meta
+          property="twitter:description"
+          content="Teste os seus conhecimentos sobre Stranger Things e vamos ver se você vai acabar parando no mundo invertido"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.ibb.co/LJXFBwQ/bgImage.png?resize=1530%2C1220&ssl=1"
+        />
         <title>{db.title}</title>
 
-        <link rel="icon" href="https://lh3.googleusercontent.com/proxy/lTNC5esOwgrxYVEks7wdiVZbkS_-cGIi84l1zvz_CVP_PUa9zIsCqz6_PZD1g5Rk8VF3HaC8jtVyWSzRUuDNA__3GBBsu66Dg9smg4fIOoyl0g" />
+        <link
+          rel="icon"
+          href="https://lh3.googleusercontent.com/proxy/lTNC5esOwgrxYVEks7wdiVZbkS_-cGIi84l1zvz_CVP_PUa9zIsCqz6_PZD1g5Rk8VF3HaC8jtVyWSzRUuDNA__3GBBsu66Dg9smg4fIOoyl0g"
+        />
 
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1YEGYNJV92"
+        />
+        <script>
+          dangerouslySetInnerHTML=
+          {{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1YEGYNJV92', { page_path: window.location.pathname });
+            `,
+          }}
+        </script>
       </Head>
       <QuizContainer>
         <QuizLogoIndex />
@@ -92,20 +134,25 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
-            <form onSubmit={function (infosDoEvento) {
-              infosDoEvento.preventDefault();
-              router.push(`/quiz?name=${name}`);
-            }}
+            <form
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+              }}
             >
               <Input
                 name="nomeDoUsuario"
-                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                onChange={(infosDoEvento) =>
+                  setName(infosDoEvento.target.value)
+                }
                 placeholder="Diz aí seu nome"
                 value={name}
               />
               <p disabled={name.length === 0} data-tip data-for="info">
                 <ReactTooltip id="info" type="light" place="top" effect="solid">
-                  <span disabled={name.length === 0}>Digite seu nome para liberar o jogo</span>
+                  <span disabled={name.length === 0}>
+                    Digite seu nome para liberar o jogo
+                  </span>
                 </ReactTooltip>
                 <Button type="submit" disabled={name.length === 0}>
                   {`Bora jogar ${name}!`}
@@ -129,12 +176,15 @@ export default function Home() {
             <h1>Quizzes da Galera</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Que tal se aventurar com diversos outros quizzes que a galera desenvolveu?</p>
+            <p>
+              Que tal se aventurar com diversos outros quizzes que a galera
+              desenvolveu?
+            </p>
             <Button
               disabled={name.length === 0}
               onClick={function (infosDoEvento) {
                 infosDoEvento.preventDefault();
-                router.push('/quizes');
+                router.push("/quizes");
               }}
             >
               Ver quizzes
